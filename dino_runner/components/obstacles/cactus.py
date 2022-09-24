7a2c3c319dab5652f640a53399d503d74b369dcf
Brayanalba08 <113906138+Brayanalba08@users.noreply.github.com>
2022-09-24 16:43:23 -0500

    Clase 2: Agregar dinosaurio

--- dino_runner/components/obstacles/cactus.py
+++
@@ -1,8 +0,0 @@
-from dino_runner.components.obstacles.obstacle import Obstacle
-import random
-
-class Cactus(Obstacle):
-    def __init__(self, image):
-        type = random.randint(0, 2)
-        super().__init__(image, type)
-        self.rect.y = 325
