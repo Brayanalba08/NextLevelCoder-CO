3fa3ae6b23a34a7d49d79d1c8d97d15310b25a3d
Brayanalba08 <113906138+Brayanalba08@users.noreply.github.com>
2022-09-27 08:29:27 -0500

    codigo completo

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,23 +1,26 @@
-from dino_runner.utils.components import SMALL_CACTUS
+from dino_runner.utils.constants import SMALL_CACTUS
 from dino_runner.components.obstacles.cactus import Cactus
 import pygame
 
 class ObstacleManager:
-
+    
     def __init__(self):
         self.obstacles = []
-
+        
     def update(self, game):
-        if len(self.obstacle) ==0:
+        if len(self.obstacles) == 0:
             new_obstacle = Cactus(SMALL_CACTUS)
             self.obstacles.append(new_obstacle)
-
+            
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if game.player.dino_rect.colliderect(obstacle.rect):
-                pygame.time.delay(500)
-                game.playing = False
-                break;
+                if game.player.shield:
+                    self.obstacles.remove(obstacle)
+                else:
+                    pygame.time.delay(500)
+                    game.playing = False
+                    break;  
 
     def draw(self, screen):
         for obstacle in self.obstacles:
